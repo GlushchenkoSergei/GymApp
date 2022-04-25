f702c20467c7c03180e7e0ad463c1f432ddedda5
Sergei <glushenko6314@icloud.com>
2022-04-25 18:43:45 +0300

    Растановка констрейнтов экрана настроек

---
+++ GymApp/ViewControllers/ChangeDetailController.swift
@@ -0,0 +1,41 @@
+//
+//  ChangeDetailController.swift
+//  GymApp
+//
+//  Created by mac on 25.04.2022.
+//
+//
+//import UIKit
+//
+//class ChangeDetailController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//
+//    @IBOutlet var changeTableView: UITableView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        changeTableView.
+//    }
+//
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+//}
